bc22a4cd3aade51e420e18bbbea80cf72457bd9d
LuffyKing <aderinwale17@gmail.com>
2022-08-21 12:19:35 +0100

    fixed issues with count and modules

--- outputs.tf
+++ outputs.tf
@@ -3,5 +3,5 @@
 #########
 
 output "cloudfront_domain_name" {
-  value = local.enable_tf_next == 0 ? null : module.tf_next.cloudfront_domain_name
+  value = local.enable_tf_next == 0 ? null : module.tf_next[0].cloudfront_domain_name
 }
